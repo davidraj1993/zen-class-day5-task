05f621972078468914a30d2075c7f83d96357c2b
davidraj <davidrajbca@gmail.com>
2024-04-26 20:00:36 +0530

    day4 task

---
+++ resume.js
@@ -0,0 +1,14 @@
+console.log("personal details");
+
+let nam = {
+            name:'Davidraj.S',
+            age:31,
+            gender :'male',
+            qualification :'mca' 
+                  
+          }
+let view1 = JSON.stringify(nam);
+let view = JSON.parse(view1);
+
+console.log(view);
+
